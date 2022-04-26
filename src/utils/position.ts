6df16dbc1a68064de50b04a01e221bb3bf90cedc
Jonah Lin <57741810+linjonah@users.noreply.github.com>
2022-04-26 16:39:40 -0700

    delete unused files

--- src/utils/position.ts
+++
@@ -1,5 +0,0 @@
-import { Position } from './../../generated/schema';
-import { Address, BigInt, ethereum } from '@graphprotocol/graph-ts'
-
-// @TO-DO: create function to retrieve position by market address + positionId
-// function getPositionByMarket(event: ethereum.Event, positionId: BigInt): Position | null {}
--- src/utils/token.ts
+++
@@ -1,4 +0,0 @@
-import { ERC20 } from './../../generated/OverlayV1Factory/ERC20'
-import { ERC20NameBytes } from './../../generated/OverlayV1Factory/ERC20NameBytes'
-import { ERC20SymbolBytes } from './../../generated/OverlayV1Factory/ERC20SymbolBytes'
-import { BigInt, Address } from '@graphprotocol/graph-ts'
