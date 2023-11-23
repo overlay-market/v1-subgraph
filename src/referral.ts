9517a899d3825f0bb5686053e6a57370cc570c8e
TomasCImach <tomas90@gmail.com>
2023-11-23 15:43:26 -0300

    add loadReferralPosition handler

--- src/referral.ts
+++ src/referral.ts
@@ -1,13 +1,16 @@
 import { Address, BigInt, ethereum } from '@graphprotocol/graph-ts'
-import { ReferralProgram } from '../generated/schema'
-import { ReferralList } from '../generated/ReferralList/ReferralList'
+import { ReferralProgram, ReferralPosition } from '../generated/schema'
+import { ReferralList, AllowAffiliates } from '../generated/ReferralList/ReferralList'
+import { ZERO_BI } from './utils/constants'
+import { loadAccount } from './utils'
+
 
 export function loadReferralProgram(event: ethereum.Event, referralAddress: Address): ReferralProgram {
-    let referralProgram = ReferralProgram.load(referralAddress.toHexString())
+    let referralProgram = ReferralProgram.load(referralAddress)
   
     // create new referralProgram if null
     if (referralProgram === null) {
-      referralProgram = new ReferralProgram(referralAddress.toHexString())
+      referralProgram = new ReferralProgram(referralAddress)
       let referralContract = ReferralList.bind(referralAddress)
 
       referralProgram.createdAtTimestamp = event.block.timestamp
@@ -16,9 +19,29 @@ export function loadReferralProgram(event: ethereum.Event, referralAddress: Addr
       referralProgram.rewardToken = referralContract.rewardToken();
       referralProgram.affiliateComission = referralContract.affiliateComission();
       referralProgram.traderDiscount = referralContract.traderDiscount();
+      referralProgram.totalRewards = ZERO_BI;
+      referralProgram.totalAirdropped = ZERO_BI;
 
       referralProgram.latestUpdateTransaction = event.transaction.hash.toHexString();
     }
   
     return referralProgram
+}
+
+export function loadReferralPosition(referralProgram: Address, owner: Address): ReferralPosition {
+    const account = loadAccount(owner)
+    account.save() // ensure account exists
+
+    let referralPosition = ReferralPosition.load(referralProgram.concat(owner))
+    if (referralPosition == null) {
+        referralPosition = new ReferralPosition(referralProgram.concat(owner))
+        referralPosition.referralProgram = referralProgram
+        referralPosition.owner = account.id
+        referralPosition.isAffiliate = false
+        referralPosition.totalAffiliateComission = ZERO_BI;
+        referralPosition.totalTraderDiscount = ZERO_BI;
+        referralPosition.totalAirdroppedAmount = ZERO_BI;
+        referralPosition.totalRewardsPending = ZERO_BI;
+    }
+    return referralPosition
 }
