4631d496798f3e34d9a8254d39edf337fcc37620
TomasCImach <tomas90@gmail.com>
2023-11-28 10:59:57 -0300

    update constants for sepolia

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -4,16 +4,10 @@ import { OverlayV1Factory as FactoryContract } from '../../generated/OverlayV1Fa
 import { OverlayV1State as StateContract } from '../../generated/OverlayV1Factory/OverlayV1State'
 
 export const ADDRESS_ZERO = '0x0000000000000000000000000000000000000000'
-// export const FACTORY_ADDRESS = '0x9a74758c2A80fA1B1d899E0E1f24CF505a4Dea33' // ethereum
-export const FACTORY_ADDRESS = '0xfa39cde07ff63b4329a70784c0600da38cf4777c' // arbitrum
-// export const PERIPHERY_ADDRESS = '0x477122219aa1F76E190f480a85af97DE0A643320' // ethereum
-export const PERIPHERY_ADDRESS = '0xc3cb99652111e7828f38544e3e94c714d8f9a51a' // arbitrum
-// periphery deployed on mainnet block 15626703
-
-export const OVL_ADDRESS = '0x4305c4bc521b052f17d389c2fe9d37cabeb70d54' // arb
-// export const OVL_ADDRESS = '0xdc77aCC82ccE1Cc095CbA197474Cc06824adE6F7' // eth
-
-export const REFERRAL_ADDRESS = '0x0000000000000000000000000000000000000000'
+export const FACTORY_ADDRESS = '0xa2dbe262d27647243ac3187d05dbf6c3c6ecc14d' // arbitrum-sepolia
+export const PERIPHERY_ADDRESS = '0x2878837EA173e8BD40Db7CEE360b15c1C27dEB5A' // arbitrum-sepolia
+export const OVL_ADDRESS = '0x3E27fAe625f25291bFda517f74bf41DC40721dA2' // arb
+export const REFERRAL_ADDRESS = '0x426cB483cad253aa7E514c6C2B76a3c215d9065b'
 
 export let ZERO_BI = BigInt.fromI32(0)
 export let ONE_BI = BigInt.fromI32(1)
