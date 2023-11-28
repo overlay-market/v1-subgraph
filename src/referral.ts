d2281fab70c8ee4dea9a768efe40cf73f3621e61
TomasCImach <tomas90@gmail.com>
2023-11-28 11:10:19 -0300

    fix: transaction null

--- src/referral.ts
+++ src/referral.ts
@@ -2,7 +2,7 @@ import { Address, BigInt, ethereum } from '@graphprotocol/graph-ts'
 import { ReferralProgram, ReferralPosition } from '../generated/schema'
 import { ReferralList, AllowAffiliates, AddAffiliate, SetRewardToken, SetAffiliateComission, SetTraderDiscount } from '../generated/ReferralList/ReferralList'
 import { ZERO_BI, REFERRAL_ADDRESS, BPS_BASE_BI } from './utils/constants'
-import { loadAccount } from './utils'
+import { loadAccount, loadTransaction } from './utils'
 
 export function handleAllowAffiliates(event: AllowAffiliates): void {
     const newAffiliates = event.params.affiliates
@@ -22,26 +22,39 @@ export function handleAddAffiliate(event: AddAffiliate): void {
 export function handleSetRewardToken(event: SetRewardToken): void {
     const referralProgram = loadReferralProgram(event, event.address)
     referralProgram.rewardToken = event.params.rewardToken
+    let transaction = loadTransaction(event)
+    transaction.save()
+
+    referralProgram.latestUpdateTransaction = transaction.id;
     referralProgram.save()
 }
 
 export function handleSetAffiliateComission(event: SetAffiliateComission): void {
     const referralProgram = loadReferralProgram(event, event.address)
     referralProgram.affiliateComission = event.params.affiliateComission
+    let transaction = loadTransaction(event)
+    transaction.save()
+
+    referralProgram.latestUpdateTransaction = transaction.id;
     referralProgram.save()
 }
 
 export function handleSetTraderDiscount(event: SetTraderDiscount): void {
     const referralProgram = loadReferralProgram(event, event.address)
     referralProgram.traderDiscount = event.params.traderDiscount
+    let transaction = loadTransaction(event)
+    transaction.save()
+
+    referralProgram.latestUpdateTransaction = transaction.id;
     referralProgram.save()
 }
 
 export function updateReferralRewards(event: ethereum.Event, owner: Address, transferFeeAmount: BigInt): void {
     const ownerReferralPosition = loadReferralPosition(Address.fromString(REFERRAL_ADDRESS), owner)
-    if (ownerReferralPosition.affiliatedTo) {
+    const affiliatedTo = ownerReferralPosition.affiliatedTo
+    if (affiliatedTo) {
         const referralProgram = loadReferralProgram(event, Address.fromString(REFERRAL_ADDRESS))
-        const affiliateReferralPosition = loadReferralPosition(Address.fromString(REFERRAL_ADDRESS), Address.fromString(ownerReferralPosition.affiliatedTo))
+        const affiliateReferralPosition = loadReferralPosition(Address.fromString(REFERRAL_ADDRESS), Address.fromString(affiliatedTo))
 
         const traderDiscount = transferFeeAmount.times(referralProgram.traderDiscount).div(BPS_BASE_BI)
         ownerReferralPosition.totalTraderDiscount = ownerReferralPosition.totalTraderDiscount.plus(traderDiscount)
@@ -75,8 +88,10 @@ export function loadReferralProgram(event: ethereum.Event, referralAddress: Addr
       referralProgram.traderDiscount = referralContract.traderDiscount();
       referralProgram.totalRewards = ZERO_BI;
       referralProgram.totalAirdropped = ZERO_BI;
+      let transaction = loadTransaction(event)
+      transaction.save()
 
-      referralProgram.latestUpdateTransaction = event.transaction.hash.toHexString();
+      referralProgram.latestUpdateTransaction = transaction.id;
     }
   
     return referralProgram
