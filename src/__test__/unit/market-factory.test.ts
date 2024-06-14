96e9e6b7dd707ee351c9a55b1bdfea41fd71b22a
Arthur G. <me@arthurka.eu>
2024-06-14 14:29:17 +0200

    test: add `FeeRecipientUpdated` handler test

--- src/__test__/unit/market-factory.test.ts
+++ src/__test__/unit/market-factory.test.ts
@@ -12,16 +12,19 @@ import { ethereum, Address, BigInt } from "@graphprotocol/graph-ts"
 
 import {
     MarketDeployed as MarketDeployedEvent,
+    FeeRecipientUpdated as FeeRecipientUpdatedEvent
 } from "../../../generated/OverlayV1Factory/OverlayV1Factory"
 import { handleFeeRecipientUpdated, handleMarketDeployed } from "../../mapping"
 import { FACTORY_ADDRESS, PERIPHERY_ADDRESS } from "../../utils/constants"
 import { setupMarketMockedFunctions } from "./shared/mockedFunctions"
+import { loadFactory } from "../../utils"
 
 export { handleMarketDeployed }
 
 const market = Address.fromString("0x0000000000000000000000000000000000000001")
 const user = Address.fromString("0x0000000000000000000000000000000000000004");
 const feed = Address.fromString("0x0000000000000000000000000000000000000005");
+const recipientAddress = Address.fromString("0x0000000000000000000000000000000000000006");
 
 const factoryAddress = Address.fromString(FACTORY_ADDRESS)
 const marketStateAddress = Address.fromString(PERIPHERY_ADDRESS)
@@ -42,6 +45,29 @@ describe("Market Factory events", () => {
 
         test("creates Market entity", () => {
             assert.entityCount("Market", 1)
+        })
+
+    })
+
+    describe("Fee Recipient Updated event", () => {
+        beforeEach(() => {
+            // initializing factory
+            loadFactory(factoryAddress.toString())
+            // imitating an event
+            const event = createFeeRecipientUpdatedEvent(factoryAddress, user, recipientAddress)
+            handleFeeRecipientUpdated(event)
+        })
+
+        afterEach(() => {
+            clearStore()
+        })
+
+        test("doesn't create additional Factory entity", () => {
+            assert.entityCount("Factory", 1)
+        })
+
+        test("fee recipient is set correctly", () => {
+            assert.fieldEquals("Factory", factoryAddress.toHexString(), "feeRecipient", recipientAddress.toHexString())
         })
     })
 })
@@ -65,6 +91,26 @@ function createMarketDeployedEvent(
     )
     event.parameters.push(
         new ethereum.EventParam("feed", ethereum.Value.fromAddress(feed)),
+    )
+
+    return event
+}
+
+function createFeeRecipientUpdatedEvent(
+    factory: Address,
+    user: Address,
+    recipient: Address
+): FeeRecipientUpdatedEvent {
+    const event = changetype<FeeRecipientUpdatedEvent>(newMockEvent())
+
+    event.address = factory
+    event.parameters = new Array()
+
+    event.parameters.push(
+        new ethereum.EventParam("user", ethereum.Value.fromAddress(user)),
+    )
+    event.parameters.push(
+        new ethereum.EventParam("recipient", ethereum.Value.fromAddress(recipient)),
     )
 
     return event
